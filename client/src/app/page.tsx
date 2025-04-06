af4a72c6fb03b7086b1328a7fae6c0221fc25fd9
Haris Khawja <hariskhawja@gmail.com>
2025-04-06 02:20:57 -0400

    loading screen fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -328,7 +328,7 @@ export default function Home() {
   if (isLoading)
     return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-        <p className="text-gray-600">Loading...</p>
+        <p className="text-gray-600">Loading... Be patient, our server takes a while to spring up!</p>
       </div>
     )
 
